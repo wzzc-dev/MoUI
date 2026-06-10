14c9cb66eca1a73851ad9ff180a3b430e61388d4
wzzc-dev <wzzc@wzzc.dev>
2026-06-10 20:59:35 +0800

    feat(bench): add skia cached layer benchmark

---
+++ moui/tests/skia_cached_layer_benchmark/native/bench_clock.c
@@ -0,0 +1,36 @@
+#define _POSIX_C_SOURCE 199309L
+
+#include <moonbit.h>
+
+#include <stdint.h>
+
+#if defined(_WIN32)
+#include <windows.h>
+#elif defined(__APPLE__)
+#include <mach/mach_time.h>
+#else
+#include <time.h>
+#endif
+
+MOONBIT_FFI_EXPORT
+double moui_skia_cached_layer_benchmark_now_ms(void) {
+#if defined(_WIN32)
+  LARGE_INTEGER frequency;
+  LARGE_INTEGER counter;
+  QueryPerformanceFrequency(&frequency);
+  QueryPerformanceCounter(&counter);
+  return ((double)counter.QuadPart * 1000.0) / (double)frequency.QuadPart;
+#elif defined(__APPLE__)
+  static mach_timebase_info_data_t timebase = {0, 0};
+  if (timebase.denom == 0) {
+    mach_timebase_info(&timebase);
+  }
+  uint64_t ticks = mach_absolute_time();
+  double ns = (double)ticks * (double)timebase.numer / (double)timebase.denom;
+  return ns / 1000000.0;
+#else
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return (double)ts.tv_sec * 1000.0 + (double)ts.tv_nsec / 1000000.0;
+#endif
+}
